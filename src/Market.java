bc2d6bd65fe18625a2c77e61ad40fdf2f47b11e0
Hanaalnuzhah <Hanaalnuzha2@gmail.com>
2022-04-30 17:22:44 -0400

    threads Assignment

--- src/Market.java
+++ src/Market.java
@@ -10,64 +10,68 @@ class Market1 {
         this.capacity=capacity;
 
     }
-    synchronized void buy(String fruit) {
+     void buy(String fruit) {
         while(true) {
-            if (!isEmpty(fruit)) {
-                System.out.println(Thread.currentThread().getName() + "  " + fruit + " is Empty ");
+            synchronized (this) {
+                if (!isEmpty(fruit)) {
+                    System.out.println(Thread.currentThread().getName() + "  " + fruit + " is Empty ");
 
-                try {
-                    wait();
-                } catch (Exception e) {
-                    System.out.println(e);
+                    try {
+                        wait();
+                    } catch (Exception e) {
+                        System.out.println(e);
+                    }
+
+                }
+                if (isEmpty(fruit)) {
+                    System.out.println(Thread.currentThread().getName() + "  " + fruit + " is going to buy");
+                    fruits.remove(fruit);
+                    System.out.println(Thread.currentThread().getName() + "  " + fruit + " is bought  ");
+                    notifyAll();
+                    break;
+                }
+            }
+
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+     void provides(String fruit) {
+        while (true) {
+            synchronized (this) {
+                if (isMarketFull()) {
+                    System.out.println(Thread.currentThread().getName() + "  " + "market  is full now which the capacity is   " + capacity + ", market can't accept more " + fruit);
+                    try {
+                        wait();
+                    } catch (Exception e) {
+                    }
                 }
 
-            }
-            if (isEmpty(fruit)) {
-                System.out.println(Thread.currentThread().getName() + "  " + fruit + " is going to buy");
-                fruits.remove(fruit);
-                System.out.println(Thread.currentThread().getName() + "  " + fruit + " is bought  ");
+                System.out.println(Thread.currentThread().getName() + "  " + fruit + " going to provided");
+                fruits.add(fruit);
+                System.out.println(Thread.currentThread().getName() + "  " + fruit + " is provided now  ");
                 notifyAll();
                 break;
             }
 
         }
-
-
-
-
-
-
-
-
     }
-
-
-
-
-    synchronized void provides(String fruit) {
-        while (true) {
-            if (isMarketFull()) {
-                System.out.println(Thread.currentThread().getName() + "  " + "market  is full now which the capacity is   " + capacity + ", market can't accept more " + fruit);
-                try {
-                    wait();
-                } catch (Exception e) {
-                }
-            }
-
-            System.out.println(Thread.currentThread().getName() + "  " + fruit + " going to provided");
-            fruits.add(fruit);
-            System.out.println(Thread.currentThread().getName() + "  " + fruit + " is provided now  ");
-            notifyAll();
-            break;
-
-        }
-    }
-    synchronized Boolean  isMarketFull(){
+     Boolean  isMarketFull(){
         if (fruits.size()==capacity)
             return true;
         return false;
     }
-    synchronized Boolean isEmpty(String fruit){
+     Boolean isEmpty(String fruit){
         return fruits.contains(fruit);
     }
 
@@ -80,7 +84,7 @@ class Market1 {
 
 
 public class Market {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         String apple = new String("apple");
         String orange = new String("orange");
         String grape = new String("grape");
@@ -101,13 +105,14 @@ public class Market {
                 said.provides(orange);
             }
         }.start();
-        new Thread() {
+        Thread t1=new Thread() {
             @Override
             public void run() {
 
                 said.buy(apple);
             }
-        }.start();
+        };
+        t1.start();
         Thread t2= new Thread() {
             @Override
             public void run() {
@@ -151,6 +156,9 @@ public class Market {
        t.setName("APPPLE BUY ");
        t.start();
 
+       t1.join();
+       t.join();
 
         }
+
 }
